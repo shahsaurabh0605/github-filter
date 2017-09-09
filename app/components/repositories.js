0acbe3e19e7373ff49c4e2f879d3278abb978298
shahsaurabh0605 <shahsaurabh0103@gmail.com>
2017-09-10 02:03:44 +0530

    styling for languages

--- app/components/repositories.js
+++ app/components/repositories.js
@@ -1,35 +1,26 @@
 import React, { Component } from "react"
-
+import language_color from "../fixtures/languages"
 let Repositories = (props) => {
     return (
         <li className="col s4">
-                <div className="">
-                    <div className="card">
-                        <div className="card-image">
-                            <img src={props.avatar}/>
-                        </div>
-                        <div className="username">{props.name}</div>
-                        <div className="card-content">
-                            <p>{props.description}</p>
-                            <div>{props.language}</div>
-                        </div>
-                        <div className="card-action">
-                            <div className="row">
-                                <div className="col s6">
-                                    <i className="fa fa-star" aria-hidden="true"></i>{props.stars}
-                                </div>
-                                <div className="col s6">
-                                    <i className="fa fa-code-fork fa-lg" aria-hidden="true"></i>{props.forks}
-                                </div>
-                            </div>
-                        </div>
+            <div className="">
+                <div className="card">
+                    <div className="card-image">
+                        <img src={props.avatar} />
+                    </div>
+                    <div className="username">{props.name}</div>
+                    <div className="card-content">
+                        <p>{props.description}</p>
+                    </div>
+                    <div className="card-action">
+                        <div><i className="fa fa-circle" aria-hidden="true" style={{ "color": language_color[props.language] }}></i>&nbsp;{props.language}</div>
+                        <div><i className="fa fa-star" aria-hidden="true"></i>{props.stars}</div>
+                        <div><i className="fa fa-code-fork fa-lg" aria-hidden="true"></i>{props.forks}</div>
                     </div>
                 </div>
+            </div>
         </li >)
 }
 
 module.exports = Repositories;
-
-
-
 
